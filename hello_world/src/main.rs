f3d95505d6b50d9be54aef0fe162839b73e5cd47
Junbo Zheng <3273070@qq.com>
2023-02-19 12:53:03 +0800

    hello_world: add sum examples
    Signed-off-by: Junbo Zheng <3273070@qq.com>

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,17 @@
+/// used for add two nums
+///
+/// # examples
+/// ```
+/// let x = sum(1,2);
+/// ```
+fn sum(a: i32, b: i32) -> i32 {
+    println!("sum for a + b = {}", a + b);
+    // let s = a + b;
+    // s
+    return a + b;
+}
+
 fn main() {
+    println!("{}", sum(10, 20));
     println!("Hello, world!");
 }
